1b366c97504cecfe8e25b39a19edfdfef79d248e
Darryl Gibbs <services@dgibbs.me>
2024-12-07 15:38:27 -0300

    complete Ansible playbook tut

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733592702',resources: [
+    cacheName: 'chirpy-1733596657',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
