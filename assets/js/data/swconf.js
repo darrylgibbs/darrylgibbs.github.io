4454842bc745dc786d83d1a1bf8df3745b003ee8
Darryl Gibbs <services@dgibbs.me>
2024-12-10 09:46:50 -0300

    homelab post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733597102',resources: [
+    cacheName: 'chirpy-1733834766',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
